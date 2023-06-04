de3fa9f3186de1624de9fd39abb8d079e19a2dcc
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-06-04 22:07:45 +0530

    Add. OSModules methods are logged.

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("This is app.js")
+const osModule = require('os');
+
+console.log(osModule.tmpdir());
+console.log(osModule.endianness());
+console.log(osModule.hostname());
+console.log(osModule.type());
+console.log(osModule.platform());
+console.log(osModule.arch());
+console.log(osModule.arch());
+console.log(osModule.release());
+console.log(osModule.uptime());
+console.log(osModule.loadavg());
+console.log(osModule.cpus());
+console.log(osModule.networkInterfaces());
+console.log(osModule.totalmem());
+console.log(osModule.freemem());
